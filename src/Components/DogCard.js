507371863a896fa464ba967f7eb2963a7e3d8418
Bennimaru <bennywhyu@gamil.com>
2019-03-17 18:22:59 -0400

    Done.

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 class DogCard extends React.Component {
+
+  state={
+    bark: false
+  }
+
+  handleClick = ()=>{
+    this.setState({
+      bark: !this.state.bark
+    })
+  }
+
   render() {
     return (
       <div>
-        <h2>{/*Dog name goes here*/}</h2>
-        <img alt="" src={""} />
-        <button onClick={""}>Bark</button>
+        <h2>{this.props.dog.name}</h2>
+        <img alt="" src={this.props.dog.img} />
+        <button onClick={this.handleClick}>Bark</button>
+        {this.state.bark? <h4 className="bark">RUFF</h4>:null}
       </div>
     );
   }
--- src/Containers/DogsList.js
+++ src/Containers/DogsList.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import DogCard from '../Components/DogCard.js'
+import Dogs from '../dogs'
 
 class DogList extends Component {
+
+  renderDog = dog =>{
+    return <DogCard key={dog.id} dog={dog}/>
+  }
+
   render() {
-    return <div className="dogContainer">{/*list of dogs go here*/}</div>;
+    return <div className="dogContainer">{/*list of dogs go here*/}
+    {Dogs.map(this.renderDog)}
+    </div>;
   }
 }
 
